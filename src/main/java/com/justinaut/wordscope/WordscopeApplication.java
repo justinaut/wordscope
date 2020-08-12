1a1ee01b5f7a40e2e7bfeda261cc04f2d2564b45
Justin Wilbourne <5108185+justinaut@users.noreply.github.com>
2020-08-11 20:05:47 -0700

    Spring initialize a java gradle project

---
+++ src/main/java/com/justinaut/wordscope/WordscopeApplication.java
@@ -0,0 +1,13 @@
+package com.justinaut.wordscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordscopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justinaut/wordscope/WordscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinaut.wordscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
