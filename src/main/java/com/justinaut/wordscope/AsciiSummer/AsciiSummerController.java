5d0d719aba8e188f9df733218629a941d74debb9
Justin Wilbourne <5108185+justinaut@users.noreply.github.com>
2020-10-27 17:18:18 -0700

    Add view for AsciiSum, reroute json output to new endpoint

--- src/main/java/com/justinaut/wordscope/AsciiSummer/AsciiSummerController.java
+++ src/main/java/com/justinaut/wordscope/AsciiSummer/AsciiSummerController.java
@@ -1,14 +1,27 @@
 package com.justinaut.wordscope.AsciiSummer;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AsciiSummerController {
 
-    @RequestMapping("/asciiSum")
-    public AsciiSum handleGet(@RequestParam(name = "q", required = false) String toSum) {
+    @GetMapping(value="/asciiSum", produces="text/html")
+    public ModelAndView getView(@RequestParam(name = "q", required = false) String toSum) {
+
+        Map<String, AsciiSum> model = new HashMap<>();
+        model.put("asciiSum", AsciiSummer.sum(toSum));
+
+        return new ModelAndView("asciiSum", model);
+    }
+
+    @GetMapping(value="/asciiSum/sum", produces="application/json")
+    public AsciiSum getAsciiSum(@RequestParam(name = "q", required = false) String toSum) {
 
         return AsciiSummer.sum(toSum);
     }
--- src/test/java/com/justinaut/wordscope/AsciiSummer/AsciiSummerControllerTest.java
+++ src/test/java/com/justinaut/wordscope/AsciiSummer/AsciiSummerControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(AsciiSummerController.class)
 class AsciiSummerControllerTest {
@@ -17,8 +18,8 @@ class AsciiSummerControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void handleGet_withStringToSum() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/asciiSum?q=ABCz123"))
+    void getAsciiSum_withStringToSum() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/asciiSum/sum?q=ABCz123"))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -26,8 +27,8 @@ class AsciiSummerControllerTest {
     }
 
     @Test
-    void handleGet_emptyStringToSum() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/asciiSum?q="))
+    void getAsciiSum_emptyStringToSum() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/asciiSum/sum?q="))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -35,11 +36,32 @@ class AsciiSummerControllerTest {
     }
 
     @Test
-    void handleGet_withoutStringToSum() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/asciiSum"))
+    void getAsciiSum_withoutStringToSum() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/asciiSum/sum"))
                 .andExpect(status().isOk())
                 .andReturn();
 
         assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo("{\"input\":\"\",\"sum\":0}");
+    }
+
+    @Test
+    void getView_returnsHtmlForInput() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/asciiSum"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("asciiSum"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Enter a string to sum");
+    }
+
+    @Test
+    void getView_returnsHtmlOfOutput() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/asciiSum?q=abc"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("asciiSum"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Input: abc");
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Sum: 294");
     }
 }
